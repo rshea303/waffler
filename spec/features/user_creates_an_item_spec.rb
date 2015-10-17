99950d82d7bc6381984c1655418002edc59ea863
Edward McCarthy <efmccarthy26@gmail.com>
2015-10-17 13:08:24 -0600

    form for needs the project ID as a symbol and the Item ID right afterwards. In an array no less. My my

--- spec/features/user_creates_an_item_spec.rb
+++ spec/features/user_creates_an_item_spec.rb
@@ -8,7 +8,7 @@ describe "user" do
     click_link_or_button "Add New Item"
     fill_in "Title", with: "Create Twitter OAuth"
     fill_in "Description", with: "Must Test feature"
-    click_link_or_button "Submit"
+    click_link_or_button "Create Item"
 
     expect(page).to have_text("Create Twitter OAuth")
 
