cf0787f66e544297cd66652793768c4ef5b9349c
Edward McCarthy <efmccarthy26@gmail.com>
2015-10-17 13:23:14 -0600

    remember to find the project ID within the Item create action, because these are attached to each other

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,21 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    project = Project.find_by(id: params[:project_id])
+    # require "pry"; binding.pry
+    if @item.save
+      redirect_to project_path(project)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description)
+  end
 end
